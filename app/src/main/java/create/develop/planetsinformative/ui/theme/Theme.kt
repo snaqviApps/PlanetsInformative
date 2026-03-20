da4e3b57c79031d9838fb8858846444b92c4ed22
sayyidnaqvi <naqvi.sdsu@gmail.com>
2026-03-20 06:15:11 -0500

    structured for using sealed approach

--- app/src/main/java/create/develop/planetsinformative/ui/theme/Theme.kt
+++ app/src/main/java/create/develop/planetsinformative/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package create.develop.planetsinformative.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
